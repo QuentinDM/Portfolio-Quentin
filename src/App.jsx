2aea2f2ead20a6fac0d1e2c7e0ad06ce1d34d60e
Quentin Della mattia <qludela@gmail.com>
2026-02-18 22:56:45 +0100

    Departure of project

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import './styles/style.css';
+
+function App() {
+  return (
+    <div className="style">
+      <h1>Welcome to My React App!</h1>
+      <p>This is a simple React application with custom styling.</p>
+      <button>Click Me</button>
+      <div className="card">
+        <h2>Card Title</h2>
+        <p>This is a sample card component.</p>
+      </div>
+    </div>
+  );
+}
+
+export default App;
